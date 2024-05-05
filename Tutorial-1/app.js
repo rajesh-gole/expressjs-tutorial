8f6e5bd2b01eb23bf2dfa6d9fd5db2b9fa716ec8
RAJESH GOLE <rajeshgole96@gmail.com>
2024-05-05 19:52:20 +0530

    HTTP Methods

--- Tutorial-1/app.js
+++ Tutorial-1/app.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req,res) => {
+    res.send('Get request at Homepage');
+    res.end();
+})
+
+app.post('/', (req,res) => {
+    res.send('Post request at Homepage');
+    res.end();
+})
+
+app.put('/', (req,res) => {
+    res.send('Put request at Homepage');
+    res.end();
+})
+
+app.delete('/', (req,res) => {
+    res.send('Delete request at Homepage');
+    res.end();
+})
+
 module.exports = app;
